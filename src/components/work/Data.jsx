40f4d5b2b59a7707c37494330e680f27336b2bac
L11nas <ulevicius.linas@gmail.com>
2024-02-27 06:00:33 +0200

    deploy json

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,39 +1,41 @@
-import Work1 from '/work1.jpg';
-import Work2 from '/work1.jpg';
+import Work1 from '/hikingtourpage.jpg';
+import Work2 from '/movie.png';
 import Work3 from '/work1.jpg';
 import Work4 from '/work1.jpg';
-import Work5 from '/work1.jpg';
+import Work5 from '/game.jpg';
 
 export const projectsData = [
   {
     id: 1,
     image: Work1,
-    title: 'Web design',
+    title: 'Website',
     category: 'Web',
+    Link: 'https://l11nas.github.io/Hiking-tour-page-html-css/',
   },
   {
     id: 2,
     image: Work2,
-    title: 'Web design',
-    category: 'Banners',
+    title: 'Web',
+    category: 'web',
   },
   {
     id: 3,
     image: Work3,
-    title: 'Web design',
+    title: 'Web',
     category: 'Design',
   },
   {
     id: 4,
     image: Work4,
-    title: 'Web design',
+    title: 'Web',
     category: 'Banners',
   },
   {
     id: 5,
     image: Work5,
-    title: 'Web design',
+    title: 'Web',
     category: 'Web',
+    Link: 'https://l11nas.github.io/game/',
   },
 ];
 
